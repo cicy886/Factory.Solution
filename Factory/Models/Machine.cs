5c831133999536e1aa617efe3ad357fa795ccdf0
Sisi Vieira <cicy886@gmail.com>
2021-10-23 15:02:40 -0700

    add Name property for Machine

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -10,6 +10,7 @@ namespace Factory.Models
     }
 
     public int MachineId { get; set; }
+    public string Name { get; set; }
     public string Description { get; set; }
     public string SerialNumber { get; set; }
     public int Date { get; set; }
