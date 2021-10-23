89e0aff096f29e0e65f80bd54e7578c71ebec85d
Sisi Vieira <cicy886@gmail.com>
2021-10-23 16:14:38 -0700

    typo fix, add Create route and corresponding view

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -7,18 +7,41 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Factory.Controllers
 {
-  public class MachineController : Controller
-  {
-    private readonly FactoryContext _db;
-
-    public MachinesController(FactoryContext db)
+    public class MachinesController : Controller
     {
-      _db = db;
-    }
+        private readonly FactoryContext _db;
 
-    public ActionResult Index()
-    {
-      return View(_db.Machines.ToList());
+        public MachinesController(FactoryContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            return View(_db.Machines.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.EngineerId =
+                new SelectList(_db.Engineers, "EngineerId", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Machine machine, int EngineerId)
+        {
+            _db.Machines.Add (machine);
+            _db.SaveChanges();
+            if (EngineerId != 0)
+            {
+                _db
+                    .EngineerMachine
+                    .Add(new EngineerMachine()
+                    { EngineerId = EngineerId, MachineId = machine.MachineId });
+            }
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
-  }
 }
